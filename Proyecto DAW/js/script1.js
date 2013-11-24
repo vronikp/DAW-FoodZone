ccfc254cb5672356f57949cb918d249699e22ccb
Iván Aveiga <ijaveiga@gmail.com>
2013-11-24 16:05:47 -0500

    Mejorando lo de los platos
    Con Diana avanzamos lo de los platos, falta mejorarlo en que sean 3 por
    fila, tengo que averiguar aún lo del mapa (locales.html) ya he de
    avanzar en esto de noche.

--- Proyecto DAW/js/script1.js
+++ Proyecto DAW/js/script1.js
@@ -3,36 +3,35 @@ function inicializar(){
 }
 
 function get_platos(){
-	
     var request;
     request = new XMLHttpRequest();
     request.addEventListener('load', show_platos, false);
-    request.open("GET", "platos.xml", true);
+    request.open("GET", "xml/platos.xml", true);
     request.send();
 }
 
 function show_platos(e){
 	var xml = e.target.responseXML;
 	ls_platos = xml.getElementsByTagName("platos")[0].getElementsByTagName("plato");
-	alert(ls_platos.length);
 	for (i = 0; i < ls_platos.length; i++)
 	{
 		var p, div, ul,li;
-		
 		div = document.createElement('div');
+		div.setAttribute("id","plato");
 		ul = document.createElement('ul');
 		li1 = document.createElement('li');
 		li2 = document.createElement('li');
+		li3 = document.createElement('li');
 		li1.innerHTML = ls_platos[i].getElementsByTagName("nombre")[0].firstChild.nodeValue;
 		ul.appendChild(li1);
 		li2.innerHTML = ls_platos[i].getElementsByTagName("puntuacion")[0].firstChild.nodeValue;
 		ul.appendChild(li2);
-		div.appendChild(ul)
+		li3.innerHTML = ls_platos[i].getElementsByTagName("descripcion")[0].firstChild.nodeValue;
+		ul.appendChild(li3);
+		div.appendChild(ul);
 		
 		document.getElementById("platos").appendChild(div);
 		
 	}
 }
-
-
 window.addEventListener('load', inicializar, false);
