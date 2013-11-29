24347e36650f3f6db08a89d9ce28edfaaf4b6988
Veronica Pozo <veronica.pozo@interprofcorp.com>
2013-11-28 21:35:32 -0500

    Inicio Sesion
    Se verifica usuario y contraseña de una lista de usuarios cliente
    (Usuario.xml) y muestra las opciones (favoritos y wish list)

--- Proyecto DAW/js/cargarArchivos.js
+++ Proyecto DAW/js/cargarArchivos.js
@@ -2,6 +2,27 @@ var listRestaurante;
 
 function inicializar(){
     obtenerRestaurante();
+    btnIniciarSesion.addEventListener('click', iniciarSesion, false);
+    obtenerZona();
+    obtenerTipo();
+}
+
+function obtenerZona(){
+    var request;
+    request = new XMLHttpRequest();
+    request.addEventListener('load', mostrarZona,false);
+    request.open("GET","xml/Zona.xml");
+    request.send();
+}
+
+function mostrarZona(e){
+    xmlZona = e.target.responseXML;
+    listaZona = xmlZona.getElementsByTagName("zonas")[0].getElementsByTagName("zona");
+    for (i=0; i< listaZona.length;i++){
+        li = document.createElement('li');
+        li.innerHTML = listaZona[i].getElementsByTagName("nombre");
+        li.name= listaZona[i].getElementsByTagName("id");
+    }
 }
 
 function obtenerRestaurante(){
@@ -14,7 +35,6 @@ function obtenerRestaurante(){
 }
 
 function mostrarRestaurante(e){
-    alert(e.target.responseText);
     var xml = e.target.responseXML;
     listRestaurante = xml.getElementsByTagName("restaurantes")[0].getElementsByTagName("restaurante");
     nombreL1.innerHTML = listRestaurante[0].getElementsByTagName("nombre")[0].firstChild.nodeValue;
@@ -24,5 +44,38 @@ function mostrarRestaurante(e){
     nombreL3.innerHTML = listRestaurante[2].getElementsByTagName("nombre")[0].firstChild.nodeValue;
     resenaL3.innerHTML = listRestaurante[2].getElementsByTagName("resena")[0].firstChild.nodeValue;
 }
+
+function iniciarSesion(){
+    var request;
+    request = new XMLHttpRequest();
+    request.addEventListener('load', verificarUsuario, false);
+    request.open("GET", "xml/Usuarios.xml", true); //deberia hacerse con POST
+    request.send();
+}
+
+function verificarUsuario(e){
+    xmlUsuarios = e.target.responseXML;
+    listaUsuarios = xmlUsuarios.getElementsByTagName("usuarios")[0].getElementsByTagName("usuario");
+
+    for (i=0; i<listaUsuarios.length;i++){
+        if(listaUsuarios[i].getElementsByTagName("nombre")[0].firstChild.nodeValue==document.getElementsByName("txtUsuario")[0].value){
+            if(listaUsuarios[i].getElementsByTagName("activo")[0].firstChild.nodeValue=="true"){
+                if (listaUsuarios[i].getElementsByTagName("clave")[0].firstChild.nodeValue == document.getElementsByName("txtPassword")[0].value) {
+                    alert("Bienvenido, "+ listaUsuarios[i].getElementsByTagName("nombre")[0].firstChild.nodeValue);
+                    document.getElementById("login").style.display = 'none';
+                    document.getElementsByClassName("usuario")[0].style.display = 'block';
+                    document.getElementsByClassName("usuario")[1].style.display = 'block';
+                }else{
+                alert("Contrasena incorrecta");
+                }
+            }else{
+            alert("El usuario no se encuentra activo. Favor contactar con el Administrador.");
+            }
+            return;
+        }
+    }
+    alert("No existe el usuario");
+}
+
 
 window.addEventListener('load', inicializar, false);
