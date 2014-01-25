a7e8dbc332b2bf6d0fe3cf8607a8073c6f2e7afb
Iván Aveiga <ijaveiga@gmail.com>
2014-01-25 15:20:44 -0500

    Empty project MVC

--- FoodZone/FoodZone/ModeloFZ.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\iaveiga\Documents\GitHub\DAW-FoodZone\FoodZone\FoodZone\ModeloFZ.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
