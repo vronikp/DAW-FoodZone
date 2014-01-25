1f447c223d95e1427e3acee6795c5787ab9636c6
Iván Aveiga <ijaveiga@gmail.com>
2014-01-25 16:55:47 -0500

    Added Viewa
    *Index
    *Maps

---
+++ MVC_FZ/MVC_FZ/Controllers/IndexControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_FZ.Controllers
+{
+    public class IndexControl : Controller
+    {
+        //
+        // GET: /IndexControl/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
